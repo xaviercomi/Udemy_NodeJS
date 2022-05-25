906bfed3798c93a3353738dfb9bb17b089fb4f05
xcomi <xaviercomi@gmail.com>
2022-05-25 21:22:46 +0200

    First commit

---
+++ 04-app-consola/app.js
@@ -0,0 +1,12 @@
+require('colors');
+const { mostrarMenu } = require('./helpers/mensajes');
+
+console.clear()
+
+const main = async () => {
+
+  mostrarMenu();
+
+}
+
+main()
---
+++ 04-app-consola/helpers/mensajes.js
@@ -0,0 +1,12 @@
+require('colors')
+
+const mostrarMenu = () => {
+
+  console.clear()
+  console.log('=============================');
+  console.log('             Menú            ');
+  console.log('=============================');
+
+};
+
+module.exports = { mostrarMenu }
